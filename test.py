1f40fff1196f9e456f79e7b7f42ac548cbed9c48
Diego Serrano Gómez <diegosg448@gmail.com>
2020-07-03 16:23:49 -0500

    Correccion menor

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 if __name__ == "__main__":
     alumnos = {"nombre": ''}
     lista = [alumnos]
-   
+
     for registro in lista:
-        registro["nombre"] = input("ingresa el codigo del alumno: ")
-    
+        registro['nombre'] = input("ingresa el codigo del alumno: ")
+
     print(lista[:])
